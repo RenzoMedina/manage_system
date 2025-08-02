484295dfbdf454c5f27f9b2666a4d18565ea4efe
RenzoMedina <medinast30@gmail.com>
2025-08-02 17:26:07 -0400

    Fix path of enviorment

--- backend/index.php
+++ backend/index.php
@@ -4,8 +4,11 @@ $envPath = '/etc/secrets/.env';
 if (!file_exists($envPath)) {
     $envPath = __DIR__ . '/.env';
 }
-$dotenv = Dotenv\Dotenv::createImmutable(dirname($envPath), basename($envPath));
-$dotenv->load();
+
+if (file_exists($envPath)) {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname($envPath), basename($envPath));
+    $dotenv->load();
+}
 
 require_once 'app/Config/DataBase.php';
 require_once 'routes/Api.php';
